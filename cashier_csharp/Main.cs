4c5727340e61256895d010d82bf25d9e846dbbd0
macheteshuffle <jeff.li029@gmail.com>
2015-06-07 14:21:07 -0400

    initial functions

--- cashier_csharp/Main.cs
+++ cashier_csharp/Main.cs
@@ -6,7 +6,25 @@ namespace cashier_csharp
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			InitialEntry ();
+		}
+
+		public static void InitialEntry()
+		{
+			string amtDue;
+			Console.Write (System.Environment.NewLine + "What is the amount due? ");
+			Console.Write ("The amount you entered is: {0}", amtDue = Console.ReadLine());
+			AmtDueCheck (amtDue);
+		}
+
+		public static void AmtDueCheck(string amtDue)
+		{
+			string pattern = "[A-Za-z]";
+
+			if (System.Text.RegularExpressions.Regex.IsMatch(amtDue, pattern)) {
+				Console.Write (System.Environment.NewLine + "Wrong input type");
+				InitialEntry ();
+			}
 		}
 	}
 }
