a0a4883e9184495da9cd8566e730473e7ca01592
macheteshuffle <jeff.li029@gmail.com>
2015-06-21 23:22:46 -0400

    code refactoring

--- cashier_csharp/Main.cs
+++ cashier_csharp/Main.cs
@@ -6,25 +6,53 @@ namespace cashier_csharp
 	{
 		public static void Main (string[] args)
 		{
-			InitialEntry ();
+			due = AmtDueInput ();
+			tendered = AmtTenderedInput ();
+			Calculate (due, tendered);
 		}
 
-		public static void InitialEntry()
+		public static void AmtDueInput()
 		{
 			string amtDue;
 			Console.Write (System.Environment.NewLine + "What is the amount due? ");
-			Console.Write ("The amount you entered is: {0}", amtDue = Console.ReadLine());
-			AmtDueCheck (amtDue);
+			Console.Write ("The amount due is: {0}", amtDue = Console.ReadLine());
+			if (AmtCheck (amtDue) == false) {
+				AmtDueInput ();
+			} else {
+				return amtDue;
+			}
+
 		}
 
-		public static void AmtDueCheck(string amtDue)
+		public static void AmtCheck(string amt)
 		{
 			string pattern = "[A-Za-z]";
 
-			if (System.Text.RegularExpressions.Regex.IsMatch(amtDue, pattern)) {
+			if (System.Text.RegularExpressions.Regex.IsMatch (amt, pattern)) {
 				Console.Write (System.Environment.NewLine + "Wrong input type");
-				InitialEntry ();
+				return false;
+			} else {
+				return true;
 			}
+		
+		}
+
+		public static void AmtTenderedInput ()
+		{
+			string amtTendered;
+			Console.Write (System.Environment.NewLine + "What is the amount tendered?");
+			Console.Write ("The amount tendered is: {0}", amtTendered = Console.ReadLine ());
+			if (AmtCheck (amtTendered) == false) {
+				AmtTenderedInput ();
+			} else {
+				return amtTendered;
+			}
+		}
+
+
+		public static void Calculate()
+		{
+
 		}
 	}
 }
